20b739f0c7dfd6b15257dac047e2219de37c75a1
Siong-Ui Te <siongui@gmail.com>
2016-02-25 13:36:59 +0800

    concatenate js

---
+++ theme/javascript/concatjs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import os
+
+def concatJS(dirpath):
+    combinedJS = ""
+    for filename in os.listdir(dirpath):
+        if filename.endswith(".js"):
+            filepath = os.path.join(dirpath, filename)
+            print("combining " + filepath + " ...")
+            with open(filepath, "r") as f:
+                combinedJS += f.read()
+
+    return combinedJS
+
+
+if __name__ == "__main__":
+    print(concatJS("."))
+    #concatJS(".")
