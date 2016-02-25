723350195ba39c2ecfa2f303e7a4cac2af5bec37
Siong-Ui Te <siongui@gmail.com>
2016-02-25 20:14:31 +0800

    font size larger/smaller

--- theme/javascript/nanda.js
+++ theme/javascript/nanda.js
@@ -1,5 +1,4 @@
 var zhconvert = document.getElementById("zh-convert");
-
 zhconvert.onclick = function() {
   if (zhconvert.dataset.zh == "tw") {
     TongWen.trans2Simp(document);
@@ -9,5 +8,29 @@ zhconvert.onclick = function() {
     TongWen.trans2Trad(document);
     zhconvert.dataset.zh = "tw";
     setTimeout(function() {zhconvert.textContent = "简体";}, 500);
+  }
+}
+
+var fl = document.getElementById("font-larger");
+fl.onclick = function() {
+  var contents = document.querySelectorAll(".main-content");
+  for (var i = 0; i < contents.length; i++) {
+    var content = contents[i];
+    if (content.style.fontSize == "") {
+      content.style.fontSize = "1.0em";
+    }
+    content.style.fontSize = (parseFloat(content.style.fontSize) + 0.25) + "em";
+  }
+}
+
+var fs = document.getElementById("font-smaller");
+fs.onclick = function() {
+  var contents = document.querySelectorAll(".main-content");
+  for (var i = 0; i < contents.length; i++) {
+    var content = contents[i];
+    if (content.style.fontSize == "") {
+      content.style.fontSize = "1.0em";
+    }
+    content.style.fontSize = (parseFloat(content.style.fontSize) - 0.25) + "em";
   }
 }
