98cb5927bf65d314aaf456386c95c6802496eb8e
siongui <siongui@gmail.com>
2016-04-07 22:36:35 +0800

    use template

--- tool/crmn/rstmetadata.go
+++ tool/crmn/rstmetadata.go
@@ -1,19 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"text/template"
 )
 
+var rstMetadataTmpl = `{{.Title}}
+################################################################################
+
+:date: {{.Date}}
+:modified: {{.Modified}}
+:oldurl: {{.Oldurl}}
+:tags: {{.Tags}}
+:category: {{.Category}}
+:summary: {{.Summary}}
+
+
+`
+
+type RstMetadata struct {
+	Title    string
+	Date     string
+	Modified string
+	Oldurl   string
+	Tags     string
+	Category string
+	Summary  string
+}
+
 func writeRstMetadata(fRstOutput *os.File, title, path string) {
-	fmt.Fprintf(fRstOutput, title+"\n")
-	fmt.Fprintf(fRstOutput, "########################################################################\n")
-	fmt.Fprintf(fRstOutput, "\n")
-	fmt.Fprintf(fRstOutput, ":date: 2012-04-22\n")
-	fmt.Fprintf(fRstOutput, ":modified: 2012-09-02\n")
-	fmt.Fprintf(fRstOutput, ":oldurl: http://nanda.online-dhamma.net/Tipitaka/Sutta/Majjhima/"+path+"\n")
-	fmt.Fprintf(fRstOutput, ":tags: 中部, 尼柯耶, Majjhima Nikāya\n")
-	fmt.Fprintf(fRstOutput, ":category: 巴利三藏中部\n")
-	fmt.Fprintf(fRstOutput, ":summary: "+title+"\n")
-	fmt.Fprintf(fRstOutput, "\n\n")
+	meta := RstMetadata{
+		Title:    title,
+		Date:     "2012-04-22",
+		Modified: "2012-09-02",
+		Oldurl:   "http://nanda.online-dhamma.net/Tipitaka/Sutta/Majjhima/" + path,
+		Tags:     "中部, 尼柯耶, Majjhima Nikāya",
+		Category: "巴利三藏中部",
+		Summary:  title,
+	}
+
+	tmpl := template.Must(template.New("rstmeta").Parse(rstMetadataTmpl))
+	tmpl.Execute(fRstOutput, meta)
 }
