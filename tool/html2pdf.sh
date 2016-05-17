47ad4ffdf5c31f4400db9e6bb9ad0678169ab69e
siongui <siongui@gmail.com>
2016-05-17 22:42:23 +0800

    try again for Travis CI build

--- tool/html2pdf.sh
+++ tool/html2pdf.sh
@@ -7,5 +7,5 @@
 for path in $(find $1 -name "*.html")
 do
   echo -e "\033[92mProcessing ${path}\033[0m"
-  wkhtmltopdf ${path} --disable-local-file-access --disable-javascript --user-style-sheet $2 "${path%.html}.pdf"
+  wkhtmltopdf ${path} --disable-javascript --user-style-sheet $2 "${path%.html}.pdf"
 done
